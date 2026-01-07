5c2f35d962df32fbdc9b0c1992e05a0d94bbf8c1
Desolation614 <jason.gbg@gmail.com>
2026-01-07 14:18:51 -0500

    g

--- src/decompiled/osrs/c.java
+++
@@ -1,4 +0,0 @@
-package osrs;
-
-public interface c {
-}
--- src/decompiled/osrs/fI.java
+++
@@ -1,9 +0,0 @@
-package osrs;
-
-import net.runelite.mapping.ObfuscatedName;
-
-@ObfuscatedName("py")
-public final class fI {
-   @ObfuscatedName("ly")
-   static String a;
-}
